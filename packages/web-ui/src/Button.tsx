4dedc97306e7756be8e2a972e3071898893e1249
SherlockHomer <406435735@qq.com>
2023-06-18 22:50:30 +0800

    fix web-ui package.json types

--- packages/web-ui/src/Button.tsx
+++ packages/web-ui/src/Button.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export function Button(props: any) {
-  return <button onClick={() => console.log("has been click")}>click me</button>
+  return <button onClick={props.onClick}>{props.children}</button>
 }
 
 export default Button;
