ede06c3b87c51616e2cc59234aca64747b133fdd
SherlockHomer <406435735@qq.com>
2023-06-18 21:55:59 +0800

    init project & add remix app & web-ui & common ts config

---
+++ packages/web-ui/src/Button.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export function Button(props: any) {
+  return <button onClick={() => console.log("has been click")}>click me</button>
+}
+
+export default Button;
---
+++ packages/web-ui/tsup.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "tsup";
+import config from "@mono/tsconfig";
+
+export default defineConfig({
+  ...config,
+});
