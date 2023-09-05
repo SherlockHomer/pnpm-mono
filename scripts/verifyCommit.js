d4cbf737d3f2c14c9750251bcd9ef21619e38ce4
SherlockHomer <406435735@qq.com>
2023-09-05 18:48:13 +0800

    feat: add commit-msg check hook when using git commit rather than npm run commit

---
+++ scripts/verifyCommit.js
@@ -0,0 +1,28 @@
+import chalk from 'chalk';
+import { readFileSync } from 'fs';
+import path from 'path';
+
+const msgPath = path.resolve('.git/COMMIT_EDITMSG');
+const msg = readFileSync(msgPath, 'utf-8').trim();
+
+const commitRE =
+  /^(revert|feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,95}/;
+
+if (!commitRE.test(msg)) {
+  console.log();
+  console.error(
+    ` ${chalk.bgRed.white(' ERROR ')} `,
+    chalk.red('  invalid commit message format.\n\n'),
+
+    chalk.red(
+      'Proper commit message format is required for automated changelog generation. Examples:\n\n'
+    ),
+
+    chalk.green("feat(packageName): add 'comments' option\n"),
+    chalk.green(`fix: I fix something for all packages\n\n`),
+
+    chalk.bgYellow.white(`  Hey!  `),
+    '  Why not use "npm run commit" to help you?\n'
+  );
+  process.exit(1);
+}
