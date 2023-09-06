ca29435c664f265770f01b9a98e5f9de445fc07e
SherlockHomer <406435735@qq.com>
2023-09-06 10:18:01 +0800

    feat(eslint-vue): add eslint-vue config & add this config into vite-3d project

---
+++ configs/eslint-vue/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'plugin:vue/vue3-essential', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    jsx: true,
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {},
+};
