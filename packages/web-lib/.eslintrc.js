b539ad8b4449782fe27c63a4d9b9caed5a80c874
SherlockHomer <406435735@qq.com>
2024-01-25 21:42:12 +0800

    fix: .eslintrc should be formatted by prettier, change it into .eslintrc.js

---
+++ packages/web-lib/.eslintrc.js
@@ -0,0 +1,8 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  $schema: 'https://json.schemastore.org/eslintrc',
+  rules: {
+    'no-extend-native': 'off',
+    'no-throw-literal': 'warn',
+  },
+};
